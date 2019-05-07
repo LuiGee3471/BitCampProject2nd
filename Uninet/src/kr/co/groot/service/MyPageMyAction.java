dee5d42f6075380aa94a4db85230df5fa4f99a27
kwakhowon <howon0206@naver.com>
2019-05-07 23:58:14 +0900

    MyPageController , Action

---
+++ Uninet/src/kr/co/groot/service/MyPageMyAction.java
@@ -0,0 +1,17 @@
+package kr.co.groot.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.groot.action.Action;
+import kr.co.groot.action.ActionForward;
+
+public class MyPageMyAction implements Action {
+
+  @Override
+  public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
