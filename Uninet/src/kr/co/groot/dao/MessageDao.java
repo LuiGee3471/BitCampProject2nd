61fd70dc37f55ae78363783dadcfca56a7ef359a
kwakhowon <howon0206@naver.com>
2019-05-07 15:23:04 +0900

    Dao 함수 생성

---
+++ Uninet/src/kr/co/groot/dao/MessageDao.java
@@ -0,0 +1,17 @@
+package kr.co.groot.dao;
+
+import java.util.List;
+
+public class MessageDao {
+	public int insertMessage() {
+		return 0;
+	}
+	
+	public int deleteMessage() {
+		return 0;
+	}
+	
+	public List<Message> selectByReceiver(){
+		return null;
+	}
+}
---
+++ Uninet/src/kr/co/groot/dao/StaffDao.java
@@ -0,0 +1,37 @@
+package kr.co.groot.dao;
+
+import java.util.List;
+
+public class StaffDao {
+	public List<Staff> selectAll(){
+		
+	}
+	
+	public Staff selectStaff() {
+		return null;
+	}
+	
+	public int insertStaff() {
+		return 0;
+	}
+	
+	public int deleteStaff() {
+		return 0;
+	}
+	
+	public int updateStaff() {
+		return 0;
+	}
+	
+	public List<Staff> selectByDept(){
+		return null;
+	}
+	
+	public List<Staff> selectByName(){
+		return null;
+	}
+	
+	public Staff selectById() {
+		return null;
+	}
+}
