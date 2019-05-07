12d4f92dd704fa01c1a70efe2972beb1959159a9
LuiGee3471 <yjs3475@gmail.com>
2019-05-07 15:32:58 +0900

    Create Staff.java

---
+++ Uninet/src/kr/co/groot/dto/Staff.java
@@ -0,0 +1,105 @@
+package kr.co.groot.dto;
+
+import java.sql.Timestamp;
+
+public class Staff {
+  private int id;
+  private String staffId;
+  private String password;
+  private String email;
+  private String name;
+  private String phoneNumber;
+  private Timestamp birthday;
+  private String image;
+  private String isManager;
+  private String isAdmin;
+  private int deptId;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getStaffId() {
+    return staffId;
+  }
+
+  public void setStaffId(String staffId) {
+    this.staffId = staffId;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public Timestamp getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(Timestamp birthday) {
+    this.birthday = birthday;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public String getIsManager() {
+    return isManager;
+  }
+
+  public void setIsManager(String isManager) {
+    this.isManager = isManager;
+  }
+
+  public String getIsAdmin() {
+    return isAdmin;
+  }
+
+  public void setIsAdmin(String isAdmin) {
+    this.isAdmin = isAdmin;
+  }
+
+  public int getDeptId() {
+    return deptId;
+  }
+
+  public void setDeptId(int deptId) {
+    this.deptId = deptId;
+  }
+}
