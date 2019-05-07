e9de2f8430814fe778be2e77e0a6e0f28f8414f8
LuiGee3471 <yjs3475@gmail.com>
2019-05-07 17:28:27 +0900

    Update StaffDao.java

--- Uninet/src/kr/co/groot/dao/StaffDao.java
+++ Uninet/src/kr/co/groot/dao/StaffDao.java
@@ -1,10 +1,28 @@
 package kr.co.groot.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import kr.co.groot.dto.Staff;
 
 public class StaffDao {
+  private Connection conn;
+  private PreparedStatement pstmt;
+  private ResultSet rs;
+  private DataSource ds;
+  
+  public StaffDao() throws NamingException {
+    Context context = new InitialContext();
+    ds = (DataSource) context.lookup("java:comp/env/jdbc/mysql");
+  }
+  
 	public List<Staff> selectAll(){
 		
 	}
@@ -21,8 +39,10 @@ public class StaffDao {
 		return 0;
 	}
 	
-	public int updateStaff() {
-		return 0;
+	public int updateStaff(Staff staff) {
+	  
+	  
+		return row;
 	}
 	
 	public List<Staff> selectByDept(){
