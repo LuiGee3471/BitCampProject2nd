40f4e6fe8a2479b373e78a27a5ca6dee3761819e
LuiGee3471 <yjs3475@gmail.com>
2019-05-09 20:59:04 +0900

    Update BoardWriteAction.java

--- Uninet/src/kr/co/groot/service/BoardWriteAction.java
+++ Uninet/src/kr/co/groot/service/BoardWriteAction.java
@@ -1,7 +1,5 @@
 package kr.co.groot.service;
 
-import java.io.IOException;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,47 +17,40 @@ import kr.co.groot.dto.Staff;
 public class BoardWriteAction implements Action {
 
   @Override
-  public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+  public ActionForward execute(HttpServletRequest request,
+      HttpServletResponse response) {
     ActionForward forward = null;
     ServletContext application = request.getServletContext();
     String uploadPath = application.getRealPath("file");
-    
-    int size = 1024 * 1024 * 5; 
-   
+
+    int size = 1024 * 1024 * 5;
+
     try {
-      MultipartRequest multi = new MultipartRequest(
-          request,
-          uploadPath,
-          size,
-          "UTF-8",
-          new DefaultFileRenamePolicy()
-          );
-      
+      MultipartRequest multi = new MultipartRequest(request, uploadPath, size,
+          "UTF-8", new DefaultFileRenamePolicy());
+
       HttpSession session = request.getSession();
       Staff staff = (Staff) session.getAttribute("staff");
-      
-      
+
       int result = 0;
       Post post = new Post();
       String url = "";
       String msg = "";
-      
-      
-      
-      
-      
+
+      String title = multi.getParameter("title");
+      String content = multi.getParameter("content");
+
       post.setTitle(title);
       post.setContent(content);
       post.setWriterId(staff.getId());
       post.setBoardType(Integer.parseInt(multi.getParameter("boardType")));
-      
+
       PostDao dao = new PostDao();
       result = dao.insertPost(post);
-      
+
       forward = new ActionForward();
-     
+
       if (result > 0) {
-        forward.setRedirect(false);
         forward.setPath("/board/writeok");
       } else {
         msg = "실패하였습니다.";
@@ -69,7 +60,7 @@ public class BoardWriteAction implements Action {
         request.setAttribute("msg", msg);
         request.setAttribute("url", url);
       }
-    }catch (Exception e) {
+    } catch (Exception e) {
       System.out.println(e.getMessage());
     }
     return forward;
