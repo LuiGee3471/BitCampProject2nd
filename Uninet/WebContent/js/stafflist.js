a6a0bce4b0cde7e4c4b894c3ad189c095acbfcf7
Kang <alkalisummer@naver.com>
2019-05-15 10:09:42 +0900

    회원관리 삭제 confirm 추가

--- Uninet/WebContent/js/stafflist.js
+++ Uninet/WebContent/js/stafflist.js
@@ -125,4 +125,17 @@ $(function() {
     
     callResult(1, "search", option, textInput);
   })
+  
+  $(document).on("click","#deleteBtn",function() {
+    
+    var result = confirm("강의를 정말 삭제하시겠습니까?");
+    
+    if(result){
+      location.replace("delete?id="+$(this).siblings("#deleteId").val());
+    }else{
+      
+    }
+   
+  });
+  
 });
